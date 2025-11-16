2f11a6fb299ce50da01287e1ea3dc2b7e063d1ec
Brian Esquenazi <brianes@wix.com>
2025-11-16 15:51:00 +0000

    Actualizaciones de Torneos y Jugadores

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,8 +4,6 @@ import CreateTournament from './pages/CreateTournament';
 import TournamentDetail from './pages/TournamentDetail';
 import OrganizeTeams from './pages/OrganizeTeams';
 import TournamentResults from './pages/TournamentResults';
-import Admins from './pages/Admins';
-import PlayerStats from './pages/PlayerStats';
 import Layout from './Layout.jsx';
 
 
@@ -16,8 +14,6 @@ export const PAGES = {
     "TournamentDetail": TournamentDetail,
     "OrganizeTeams": OrganizeTeams,
     "TournamentResults": TournamentResults,
-    "Admins": Admins,
-    "PlayerStats": PlayerStats,
 }
 
 export const pagesConfig = {
